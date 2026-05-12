1f3cc9b8788c7b9c50468eb4303cfed25b706f2a
afrrooz <afrozmohammed951@gmail.com>
2026-05-12 15:34:28 +0530

    main.py is modified

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ def subtract(a,b):
 def multiply(a,b):
     return a*b  
 
+def divide(a,b):
+    if b == 0:
+        return "Error: Division by zero"
+    return a/b
+
 print (add(5,3))
 print (subtract(5,3))
 print (multiply(5,3))   
+print (divide(5,3)) 
